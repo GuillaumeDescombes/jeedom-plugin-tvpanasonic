c7b32671e234ddf7ac447d4cb819f4d5af9dcb57
Julien FABRE <jufab@users.noreply.github.com>
2017-12-29 20:44:31 +0100

    modification conf

--- desktop/php/tvpanasonic.php
+++ desktop/php/tvpanasonic.php
@@ -27,76 +27,87 @@ $eqLogics = eqLogic::byType('tvpanasonic');
 
     <!-- Affichage de l'eqLogic sélectionné -->
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <form class="form-horizontal">
-            <fieldset>
-                <legend>
-                    <!-- Retour au Général et affichage de la configuration avancée -->
-                    <i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  
-                    <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-                </legend>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Nom de l'équipement Panasonic TV}}</label>
-                    <div class="col-sm-3">
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Panasonic TV}}"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                    <div class="col-sm-3">
-                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                            <option value="">{{Aucun}}</option>
-                            <?php
-                            foreach (object::all() as $object) {
-                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label" >{{Activer}}</label>
-					<div class="col-sm-9">
-                        <label class="checkbox-inline">
-                            <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>
-                            {{Activer}}
-                        </label>
-                        <label class="checkbox-inline">
-                            <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
-                            {{Visible}}
-                        </label>
-					</div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Adresse IP TV Panasonic</label>
-                    <div class="col-sm-3">
-                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="ipTvPanasonic"/>
-                    </div>
-                </div>
-            </fieldset>
-        </form>
 
-        <legend>{{Panasonic TV}}</legend>
-        <table id="table_cmd" class="table table-bordered table-condensed">
-            <thead>
-                <tr>
-                    <th>{{Nom}}</th><th>{{Type}}</th><th>{{Action}}</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
+        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i>  {{Sauvegarder}}</a>
+        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+        <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
 
-        <form class="form-horizontal">
-            <fieldset>
-                <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                </div>
-            </fieldset>
-        </form>
+
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="return" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+            <li role="presentation" class="active"><a href="#materiel" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Matériel}}</a></li>
+            <li role="presentation"><a href="#commandes" aria-controls="commandes" role="tab" data-toggle="tab"><iclass="fa fa-list-alt"></i> {{Commandes}}</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="materiel">
+                <br />
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Nom de l'équipement Panasonic TV}}</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Panasonic TV}}"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                            <div class="col-sm-3">
+                                <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                                    <option value="">{{Aucun}}</option>
+                                    <?php
+                                    foreach (object::all() as $object) {
+                                        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label" >{{Activer}}</label>
+                            <div class="col-sm-9">
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>
+                                    {{Activer}}
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
+                                    {{Visible}}
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">Adresse IP TV Panasonic</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="ipTvPanasonic"/>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="commandes">
+                <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a>
+                <br/><br/>
+                <table id="table_cmd" class="table table-bordered table-condensed">
+                    <thead>
+                        <tr>
+                            <th>{{Nom}}</th>
+                            <th>{{Type}}</th>
+                            <th>{{Option}}</th>
+                            <th>{{Divers}}</th>
+                            <th>{{Action}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+                <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i>  {{Sauvegarder}}</a>
+            </div>
+        </div>
 
     </div>
+
 </div>
 
 <?php include_file('desktop', 'tvpanasonic', 'js', 'tvpanasonic'); ?>
